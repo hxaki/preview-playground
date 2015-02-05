fb08eb17fa186d127bb066956c50ee65b80c0841
Henry Akiyama <hen.akiyama@gmail.com>
2015-02-05 17:59:40 -0500

    something changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var el = document.querySelectorAll('a')[0];
 function spaceCheck() {
     var bodyRect = document.body.getBoundingClientRect();
     var el = document.querySelectorAll('a')[0];
@@ -27,10 +27,33 @@ function testFakkuRequest() {
 window.addEventListener("resize", function() {console.log("RESIZED");});
 //dynamically generate the preview
 
-el.addEventListener('mouseover', function() {
-    document.querySelectorAll('.fpreview')[0].style.display = "block";
-});
-el.addEventListener('mouseout', function() {
-    document.querySelectorAll('.fpreview')[0].style.display = "none";
-});
+// el.addEventListener('mouseover', function() {
+//     document.querySelectorAll('.fpreview')[0].style.display = "block";
+// });
+// el.addEventListener('mouseout', function() {
+//     document.querySelectorAll('.fpreview')[0].style.display = "none";
+// });
     
+
+var cover = document.querySelectorAll(".cover > img")[0];
+var title = document.querySelectorAll(".content > .title")[0];
+var series = document.querySelectorAll(".content > .series")[0];
+var artist = document.querySelectorAll(".content > .artist")[0];
+var desc = document.querySelectorAll(".content > .description")[0];
+var tags = document.querySelectorAll(".content > .tags")[0];
+
+request = testFakkuRequest();
+
+cover.src = "https:" + request.content.content_images.cover;
+title.innerHTML = request.content.content_name;
+for (var i = 0; i < request.content.content_series.length; i++) {
+    series.innerHTML += request.content.content_series[i].attribute;
+}
+
+for (var i = 0; i < request.content.content_artists.length; i++) {
+    artist.innerHTML += request.content.content_artists[i].attribute + " ";
+}
+
+desc.innerHTML = request.content.content_description;
+
+
